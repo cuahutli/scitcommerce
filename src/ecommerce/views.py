011c03bb43a3247a928683fe91e36f79873de619
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2018-10-19 23:15:19 -0600

    Creando el diccionario context para pasar variables de la vista al template

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -3,4 +3,23 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Hola mundo",
+        "content": "Bienvenido a la página de inicio"
+    }
+    return render(request, "home_page.html", context)
+
+
+def about_page(request):
+    context = {
+        "title": "Acerca de..",
+        "content": "Bienvenido a la página Acerca de"
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Contacto",
+        "content": "Bienvenido a la página de contacto"
+    }
+    return render(request, "home_page.html", context)
